1caaf9f7b24c72a3ffcb510e5ac5e0bff164c3f2
YazoonDinalt <kykyky228kykyk@gmail.com>
2024-10-16 17:27:52 +0300

    feat: Add ast and dune file
    Signed-off-by: YazoonDinalt <kykyky228kykyk@gmail.com>

--- MEML/lib/ast.ml
+++ MEML/lib/ast.ml
@@ -1,18 +1,18 @@
-(** Copyright 2024-2025, Perevalov Efim, Dyachkov Vitaliy *)
+(** Copyright 2024-2025, Efim Perevalov, Vetaly Dyachkov *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-type name = string [@@deriving eq, show { with_path = false }]
+type name = string [@@deriving show { with_path = false }]
 
 type const =
   | CInt of int (** integer number: ..., 0, 1, 2, ...*)
+  | CString of string (** string values: "Ocaml" *)
   | CBool of bool (** boolean values: true and false *)
-  | CNil (** [] *)
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 type binary_op =
   | Add (** 1 + 2 *)
-  | Sub (** 2 - 1 *)
+  | Sub (** 1 - 2 *)
   | Mul (** * *)
   | Div (** / *)
   | Mod (** % *)
@@ -24,44 +24,33 @@ type binary_op =
   | Gre (** > *)
   | Leq (** <= *)
   | Greq (** >= *)
-[@@deriving eq, show { with_path = false }]
-
-type type_of_var =
-  | TInt (** int type for variable *)
-  | TBool (** bool type for variable *)
-  | TUnknown (** unknown type for variable *)
-  | TArrow of type_of_var * type_of_var (** type int -> int... *)
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 type pattern =
   | PWild (** _ *)
   | PConst of const (** constant pattern *)
-  | PVar of name * type_of_var (** variable pattern*)
-  | PTuple of pattern list (** (a, b) *)
-  | PCon of pattern * pattern (** hd::tl *)
-[@@deriving eq, show { with_path = false }]
+  | PVar of name (** varuable pattern*)
+[@@deriving show { with_path = false }]
 
-type rec_flag =
+type rec_flag = 
   | Rec (** let rec v = ... *)
   | Notrec (** let z = ...*)
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 type expression =
   | EConst of const (** constant *)
-  | EVar of name * type_of_var (** variable *)
+  | EVar of name (** variable *)
   | EBinaryOp of binary_op * expression * expression (** binary operation *)
   | EApp of expression * expression (** application *)
   | EIfElse of expression * expression * expression (** if z then v else n*)
-  | ELetIn of rec_flag * name * expression * expression
+  | ELetIn of rec_flag * expression * expression
   | EFun of pattern * expression (** fun z -> z + z *)
-  | EList of expression * expression (** [1;2;3]*)
-  | ETuple of expression list (** (1,2,3)*)
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 (** Binding type *)
 type bindings =
-  | Let of rec_flag * name * expression (** let id = expr *)
-  | Expression of expression (** simple expressions *)
+  | ELet of rec_flag * name * expression (** An expression for let declaration: let id = expr *)
+  (** An expression for let rec declaration: let rec id = expr *)
 [@@deriving show { with_path = false }]
 
 (** Statements type *)
